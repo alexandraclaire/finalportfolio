526fa72878a71f913832a95ffa408c2fed63a4ff
Alexandra Carter <alexandra.carternz@gmail.com>
2015-11-30 15:47:50 +1300

    monday 30 nov

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+  $(function() {
+    $(".rslides").responsiveSlides();
+  });
+  $(".rslides").responsiveSlides({
+		auto: false,
+        pager: true,
+        nav: true,
+        speed: 800,
+        maxwidth: 800,
+        namespace: "centered-btns"
+});
