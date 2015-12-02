2f2583e64103953b2db6cf36a8e37a534b76bed9
Alexandra Carter <alexandra.carternz@gmail.com>
2015-12-02 16:12:19 +1300

    e.o.d wed 2 dec

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ $(function() {
         auto: false,             // Boolean: Animate automatically, true or false
         speed: 1200,            // Integer: Speed of the transition, in milliseconds
         timeout: 4800,          // Integer: Time between slide transitions, in milliseconds
-        pager: false,            // Boolean: Show pager, true or false
+        pager: true,            // Boolean: Show pager, true or false
         nav: true,              // Boolean: Show navigation, true or false
         random: false,          // Boolean: Randomize the order of the slides, true or false
         pause: false,           // Boolean: Pause on hover, true or false
